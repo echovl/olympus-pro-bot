7c333f16b0d8167aa80bbd9d8ad2c80f01cca8c6
Alonso Villegas <alonsovillegas816@gmail.com>
2022-05-03 13:27:01 -0500

    refactor: optimize price feed

--- src/price.ts
+++ src/price.ts
@@ -9,21 +9,28 @@ const BOND_ADDRESS = "0xd0Ed146951A189e3288d31aC815cabA619D56143"
 const DEUS_FTM_LP_ADDRESS = "0xaF918eF5b9f33231764A5557881E6D3e5277d456"
 const FTM_USDC_LP_ADDRESS = "0x2b4C76d0dc16BE1C31D4C1DC53bF9B45987Fc75c"
 
+const LP_ABI = JSON.parse(fs.readFileSync(LP_ABI_PATH).toString())
+const BOND_ABI = JSON.parse(fs.readFileSync(BOND_ABI_PATH).toString())
+
 export class PriceFeed {
+    deusFtmLp = new this.web3.eth.Contract(LP_ABI, DEUS_FTM_LP_ADDRESS)
+    ftmUsdcLp = new this.web3.eth.Contract(LP_ABI, FTM_USDC_LP_ADDRESS)
+    bond = new this.web3.eth.Contract(BOND_ABI, BOND_ADDRESS)
+
     constructor(private web3: Web3) {}
 
     async getDeusPrice(): Promise<number> {
-        const lpAbi = JSON.parse(fs.readFileSync(LP_ABI_PATH).toString())
-        const deusFtmLp = new this.web3.eth.Contract(lpAbi, DEUS_FTM_LP_ADDRESS)
-        const ftmUsdcLp = new this.web3.eth.Contract(lpAbi, FTM_USDC_LP_ADDRESS)
-
-        const deusFtmLpReserves = await deusFtmLp.methods.getReserves().call()
+        const deusFtmLpReserves = await this.deusFtmLp.methods
+            .getReserves()
+            .call()
         const ftmReserve1 = new Big(deusFtmLpReserves._reserve0)
         const deusReserve = new Big(deusFtmLpReserves._reserve1)
 
         const deusFtmPrice = ftmReserve1.div(deusReserve)
 
-        const ftmUsdcLpReserves = await ftmUsdcLp.methods.getReserves().call()
+        const ftmUsdcLpReserves = await this.ftmUsdcLp.methods
+            .getReserves()
+            .call()
         const usdcReserve = new Big(ftmUsdcLpReserves._reserve0)
         const ftmReserve2 = new Big(ftmUsdcLpReserves._reserve1)
 
@@ -33,10 +40,7 @@ export class PriceFeed {
     }
 
     async getBondPrice(): Promise<number> {
-        const bondAbi = JSON.parse(fs.readFileSync(BOND_ABI_PATH).toString())
-        const bond = new this.web3.eth.Contract(bondAbi, BOND_ADDRESS)
-
-        const price = await bond.methods.trueBondPrice().call()
+        const price = await this.bond.methods.trueBondPrice().call()
 
         return Big(price).div("1e7").toNumber()
     }
